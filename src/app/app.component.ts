e3fa6916505253db98bdcfe4ae870f2b64c15d46
daniel.luz_t <daniel.luz_t@ti.growdev.com.br>
2025-05-24 20:31:04 -0300

    Add FlexLayoutModule and update styles
    Integrated FlexLayoutModule for layout flexibility and updated styles for a cleaner UI. Modified padding, font styles, and color variables to improve consistency and maintain theme alignment. Updated `package.json` and `package-lock.json` to include the new dependency.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {MatButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MATERIAL_MODULES} from './shared/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButton, MatToolbar, ...MATERIAL_MODULES],
+  imports: [RouterOutlet, MatButton, MatToolbar, FlexLayoutModule, ...MATERIAL_MODULES],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Rick and Morty';
 }
