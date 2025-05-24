df88b53c83e96bafd4fbf9aa50aa3860e50516ce
daniel.luz_t <daniel.luz_t@ti.growdev.com.br>
2025-05-24 17:47:49 -0300

    Update header and logo in app UI
    Replaced old logo with a new SVG file and updated navigation menu labels to reflect new content. These changes improve visual consistency and provide a localized user experience.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {MatButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MATERIAL_MODULES} from './shared/material';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButton, MatToolbar, ...MATERIAL_MODULES],
+  imports: [RouterOutlet, MatButton, MatToolbar, ...MATERIAL_MODULES, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
