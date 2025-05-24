d0dd0e8e9267b6fc2bd70c546d7ea3c4d1f0eec3
daniel.luz_t <daniel.luz_t@ti.growdev.com.br>
2025-05-24 19:04:39 -0300

    Revamp styling and introduce custom Material theme
    Updated app layout with a responsive container and new styles leveraging SCSS variables. Integrated a custom Material Design theme and animations, enhancing UI consistency and accessibility. Added necessary dependencies and configuration to support these updates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import {MatButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MATERIAL_MODULES} from './shared/material';
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButton, MatToolbar, ...MATERIAL_MODULES, NgOptimizedImage],
+  imports: [RouterOutlet, MatButton, MatToolbar, ...MATERIAL_MODULES],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_THEME, createTheme, Theme } from '@angular/material/core';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+const myCustomTheme: Theme = createTheme({
+  colorSchemes: {
+    light: {
+      primary: '#11CCAB',
+      onPrimary: '#0F1E21',
+      secondary: '#4D3FA6',
+      onSecondary: '#ffffff',
+      surface: '#19262B',
+      background: '#0F1E21',
+      error: '#B00020'
+    }
+  }
+});
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideAnimations(),
+    { provide: MAT_THEME, useValue: myCustomTheme }
+  ]
 };
