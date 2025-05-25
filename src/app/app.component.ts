f930b53face80400f8628a40bdf046df0d3e7f4e
daniel.luz <daniel.luz@mirante.net.br>
2025-05-25 15:57:24 -0300

    Refactor imports and update UI for navigation components
    Made `AppComponent` standalone, adjusted imports, and updated links to use `routerLink` for improved routing functionality. Updated `AboutComponent` to include `CommonModule` and modified its template content for better presentation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-}
+export class AppComponent { }
