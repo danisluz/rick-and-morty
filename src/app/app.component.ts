9dea0f8d635c1d095dcaf62eee33a42ff473ac93
daniel.luz_t <daniel.luz_t@ti.growdev.com.br>
2025-05-26 17:37:13 -0300

    Refactor asset handling and improve SSR setup.
    Moved public assets to the `src/assets` directory for consistency and updated references accordingly. Improved server-side rendering (SSR) routes and added platform-specific logic to hide the global loader on the browser. Adjusted app build configuration for a custom base href and included global loader styling in `index.html`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {CharacterModalComponent} from './components/character-modal/character-modal.component';
 import {ToastComponent} from './shared/alert/toast.component';
 import {ToastService} from './shared/toast.service';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +16,19 @@ export class AppComponent implements OnInit{
   message: string | null = null;
   type: 'success' | 'danger' | 'warning' | 'info' = 'info';
 
-  constructor(private toastService: ToastService) {}
+  constructor(
+    private toastService: ToastService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const loader = document.getElementById('global-loader');
+      if (loader) {
+        loader.style.display = 'none';
+      }
+    }
+
     this.toastService.message$.subscribe(msg => this.message = msg);
     this.toastService.type$.subscribe(type => this.type = type);
   }
